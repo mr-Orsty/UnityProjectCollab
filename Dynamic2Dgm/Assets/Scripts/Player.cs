be54bd8d188098012401a6b87894b390f3260c7c
MrOrsty <maksiksuperkik@gmail.com>
2023-10-15 00:46:35 +0300

    15.10.23 0:46

---
+++ Dynamic2Dgm/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    PhotonView view;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        view = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (view.IsMine)
+        {
+            Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            Vector2 moveAmount = moveInput.normalized * speed * Time.deltaTime;
+            transform.position += (Vector3)moveAmount;
+        }
+
+    }
+}
--- Dynamic2Dgm/Assets/Scripts/SpawnPlayers.cs
+++ Dynamic2Dgm/Assets/Scripts/SpawnPlayers.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class SpawnPlayers : MonoBehaviour
 {
+    public GameObject player;
+    public float minX, minY, maxX, maxY;
+
     // Start is called before the first frame update
     void Start()
     {
         
+        Vector2 randomPosition = new Vector2 (Random.Range (minX, minY), Random.Range (maxX, maxY));
+        PhotonNetwork.Instantiate (player.name, randomPosition, Quaternion.identity);
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
